6c4deb3fc83a0cfc5f3a0bbd217ddde728f9d716
xiangsong <1638392300@qq.com>
2020-11-17 22:33:55 +0800

    fix

--- src/main/java/cl/ReCL2.java
+++ src/main/java/cl/ReCL2.java
@@ -1,46 +1,46 @@
-package cl;
-
-import java.io.*;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Hashtable;
-import java.util.Map;
-
-/**
- * @author Frank
- * @date 2020/5/12 1:00
- */
-public class ReCL2 extends URLClassLoader {
-
-    private Map<String, ClassLoader> classLoaderMap = new Hashtable<>();
-
-    public ReCL2(URL[] urls, ClassLoader parent) {
-        super(urls, parent);
-    }
-
-
-    @Override
-    public Class<?> loadClass(String name) throws ClassNotFoundException {
-        if(name.equals("com.company.A")){
-            String baseLibDir = System.getProperty("user.dir") + File.separator + "lib";
-            try {
-                ClassLoader loader = classLoaderMap.competeIfAbsent(name,new URLClassLoader(new URL[]{new URL("file:" + baseLibDir + File.separator + "A.jar")},null));
-                classLoaderMap.computeIfAbsent(name, k->loader);
-                return loader.loadClass(name);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        if(name.equals("com.company.B")){
-            String baseLibDir = System.getProperty("user.dir") + File.separator + "lib";
-            try {
-                ClassLoader loader = classLoaderMap.competeIfAbsent(name,new URLClassLoader(new URL[]{new URL("file:" + baseLibDir + File.separator + "B.jar")},null));
-                classLoaderMap.computeIfAbsent(name, k->loader);
-                return loader.loadClass(name);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return super.loadClass(name);
-    }
-}
+//package cl;
+//
+//import java.io.*;
+//import java.net.URL;
+//import java.net.URLClassLoader;
+//import java.util.Hashtable;
+//import java.util.Map;
+//
+///**
+// * @author Frank
+// * @date 2020/5/12 1:00
+// */
+//public class ReCL2 extends URLClassLoader {
+//
+//    private Map<String, ClassLoader> classLoaderMap = new Hashtable<>();
+//
+//    public ReCL2(URL[] urls, ClassLoader parent) {
+//        super(urls, parent);
+//    }
+//
+//
+//    @Override
+//    public Class<?> loadClass(String name) throws ClassNotFoundException {
+//        if(name.equals("com.company.A")){
+//            String baseLibDir = System.getProperty("user.dir") + File.separator + "lib";
+//            try {
+//                ClassLoader loader = classLoaderMap.competeIfAbsent(name,new URLClassLoader(new URL[]{new URL("file:" + baseLibDir + File.separator + "A.jar")},null));
+//                classLoaderMap.computeIfAbsent(name, k->loader);
+//                return loader.loadClass(name);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        if(name.equals("com.company.B")){
+//            String baseLibDir = System.getProperty("user.dir") + File.separator + "lib";
+//            try {
+//                ClassLoader loader = classLoaderMap.competeIfAbsent(name,new URLClassLoader(new URL[]{new URL("file:" + baseLibDir + File.separator + "B.jar")},null));
+//                classLoaderMap.computeIfAbsent(name, k->loader);
+//                return loader.loadClass(name);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return super.loadClass(name);
+//    }
+//}
